dae11d8db00022b0c87a2a9225752a423605f933
CristianCazares <cm_cjavier@hotmail.com>
2022-06-29 12:54:39 -0600

    feat: added score script

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PlayerScore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int _score;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "ScoreBox")
+        {
+            _score++;
+        }
     }
 }
