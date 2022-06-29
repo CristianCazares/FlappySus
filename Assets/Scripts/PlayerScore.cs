9b6fcea95a6dd25251fbf0361e4b46fb34766eae
CristianCazares <cm_cjavier@hotmail.com>
2022-06-29 13:18:03 -0600

    feat: added score on UI

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerScore : MonoBehaviour
 {
     [SerializeField] int _score;
+    [SerializeField] TextMeshProUGUI _textScore;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "ScoreBox")
         {
             _score++;
+            _textScore.text = _score.ToString();
         }
     }
 }
